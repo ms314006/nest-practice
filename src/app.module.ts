a014ce36e7b4524399d122aa3a1c1a1c07ea754c
GQSM <ms314006@hotmail.com>
2023-01-01 14:38:13 +0800

    ADD cat modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CatsModule } from './modules/cats/cats.modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule],
 })
 export class AppModule {}
